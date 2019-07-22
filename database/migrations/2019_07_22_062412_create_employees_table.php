c6858ba3a50b56399ca876f4cf2f40df59f8f16d
Erickson Reyes <ercbluemonday@yahoo.com>
2019-07-22 14:29:29 +0800

    Add employees migration script

---
+++ database/migrations/2019_07_22_062412_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('rowId');
+            $table->string('id', 150)->unique();
+            $table->string('username', 150)->unique();
+            $table->integer('password');
+            $table->timestamps();
+            $table->index(['username', 'password']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
